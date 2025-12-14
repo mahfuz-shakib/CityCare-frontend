f02b92fd8245927daa4fe8bff710b814e7d904ba
Mahfuz Shakib <mahfuzshakib301@gmail.com>
2025-12-15 03:42:37 +0600

    report issue page done

--- src/components/Form/ReportIssueForm.jsx
+++ src/components/Form/ReportIssueForm.jsx
@@ -3,9 +3,12 @@ import Container from "../../container/Container";
 import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
 import { imageUpload } from "../../utils";
+import useAuth from "../../hooks/useAuth";
+// import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const ReportIssueForm = () => {
-  const {user}=useAuth();
+  const { user } = useAuth();
+  // const axiosSecure = useAxiosSecure()
   const {
     register,
     handleSubmit,
@@ -13,19 +16,23 @@ const ReportIssueForm = () => {
   } = useForm();
   const onSubmit = async (data) => {
     const imageURL = await imageUpload(data?.image[0]);
-    const {title,category,description,location}=data;
-    const issueInfo={
-          title,
-          category,
-          description,
-          location,
-          image:imageURL,
-          reporter:{
-            name:user.diaplayName,
-            email:user.email,
-            photo:user.photoURL
-          }
-    }
+    const { title, category, description, location } = data;
+    const issueInfo = {
+      title,
+      category,
+      description,
+      location,
+      image: imageURL,
+      reporter: {
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      },
+    };
+
+    // cosnt {data:}
+
+    console.log(issueInfo);
   };
 
   return (
@@ -37,43 +44,51 @@ const ReportIssueForm = () => {
         transition={{ duration: 0.5 }}
         className=" max-w-76 md:max-w-[856px]  mx-auto card rounded-lg overflow-hidden my-16"
       >
-        <div className={`card-body px-2 bg-indigo-50`}>
+        <div className={`card-body px-2 md:px-4 bg-gray-100`}>
           <form onSubmit={handleSubmit(onSubmit)}>
             <fieldset className="! fieldset space-y-2">
               <div className="flex flex-col md:flex-row justify-between gap-5">
                 <div className="w-72 md:w-md">
-                  <label className="label">Issue Title</label>
+                  <label htmlFor="name" className="label md:text-sm">
+                    Issue Title
+                  </label>
                   <input
                     type="text"
                     id="name"
                     className="input-field"
                     placeholder="Enter issue title"
                     {...register("title", {
-                      required: "title must be required",
+                      required: "Title must be required",
                       minLength: { value: 3, message: "title atleast 3 character" },
                     })}
                   />
+                  {errors.title && <p className="mt-1 text-xs text-red-500">{errors.title.message} </p>}
                 </div>
                 <div className="w-72 md:w-xs">
-                  <label className="label">Select Category</label>
+                  <label className="label md:text-sm">Select Category</label>
                   <br />
                   <select
                     className="select select-bordered"
                     {...register("category", {
                       required: true,
+                      minLength: {
+                        value: 1,
+                        message: "Please Select category",
+                      },
                     })}
                   >
-                    <option disabled>Select category</option>
-                    <option>Food</option>
-                    <option>Pets</option>
-                    <option>Accessories</option>
-                    <option>Care Products</option>
+                    <option value="">Select Category</option>
+                    <option value="road">Road</option>
+                    <option value="water">Water</option>
+                    <option value="electricity">Electricity</option>
+                    <option value="garbage">Garbage</option>
                   </select>
+                  {errors.category && <p className="mt-1 text-xs text-red-500">{errors.category.message} </p>}
                 </div>
               </div>
               <div className="w-full flex flex-col md:flex-row gap-5 md:gap-10 justify-between">
                 <div className="w-72 md:w-md">
-                  <label className="label">Location</label>
+                  <label className="label md:text-sm">Location</label>
                   <br />
                   <input
                     type="text"
@@ -84,41 +99,39 @@ const ReportIssueForm = () => {
                       minLength: { value: 3, message: "Location atleast 3 character" },
                     })}
                   />
+                  {errors.location && <p className="mt-1 text-xs text-red-500">{errors.location.message} </p>}
+                </div>
+                <div className="w-72 md:w-xs">
+                  <div>
+                    <label className="label md:text-sm">Sample Issue Image</label>
+                    <input
+                      type="file"
+                      id="image"
+                      accept="image/*"
+                      {...register("image", {
+                        required: "Image must be required",
+                      })}
+                      className=" file-input file:bg-lime-50 file:text-lime-700"
+                    />
+                    {errors.image && <p className="mt-1 text-xs text-red-500">{errors.image.message} </p>}
+                  </div>
                 </div>
               </div>
               <div className="w-72 md:w-full">
-                <label className="label">Description</label>
+                <label className="label md:text-sm">Description</label>
                 <br />
                 <textarea
                   className="textarea textarea-bordered h-24 md:h-8 md:w-full"
                   placeholder="Enter description"
-                     {...register("description", {
-                          required: "Description must be required",
-                          minLength: { value: 3, message: "Description atleast 10 character" },
-                        })}
+                  {...register("description", {
+                    required: "Description must be required",
+                    minLength: { value: 3, message: "Description atleast 10 character" },
+                  })}
                 ></textarea>
+                {errors.description && <p className="mt-1 text-xs text-red-500">{errors.description.message} </p>}
               </div>
-              <div className="w-72 md:w-full">
-                <div>
-                  <label className="label">Profile Image</label>
-                  <input
-                    type="file"
-                    id="image"
-                    accept="image/*"
-                    {...register("image")}
-                    className="block w-full text-sm text-gray-500
-                      file:mr-4 file:py-2 file:px-4
-                      file:rounded-md file:border-0
-                      file:text-sm file:font-semibold
-                    file:bg-lime-50 file:text-lime-700
-                    hover:file:bg-lime-100
-                    bg-gray-100 border border-dashed border-lime-300 rounded-md cursor-pointer
-                      focus:outline-none focus:ring-2 focus:ring-lime-400 focus:border-lime-400
-                      py-2"
-                  />
-                </div>
-              </div>
-              <button className={`btn mx-auto w-72  md:w-sm  text-white mt-4 hover:bg-purple-800  "bg-grad"`}>
+
+              <button className={`btn mx-auto w-72  md:w-sm  text-whit mt-4 hover:bg-purple-800  "bg-grad"`}>
                 Report Issue
               </button>
             </fieldset>
--- src/pages/Dashboard/Citizen/MyIssues.jsx
+++ src/pages/Dashboard/Citizen/MyIssues.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const MyIssues = () => {
+    // const axiosSecure = useAxiosSecure()
+    const {user} = useAuth
+    // const {data:issues=[]}=useQuery({
+    //     queryKey:['issues']
+    //     queryFn:()=>
+    // })
     return (
         <div>
             <h1>citizen: my issues</h1>
